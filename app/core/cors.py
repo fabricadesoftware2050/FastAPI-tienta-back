968b19ee68084c4e1e4ff3464f972f3290ffe1e4
Christian Camilo Serna Gamez <christian.sernag@bam.com.gt>
2025-12-12 17:50:43 -0500

    Nueva URL

--- app/core/cors.py
+++ app/core/cors.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["https://cafedelvalle.vercel.app"],
+        allow_origins=["https://vuetienta-front.vercel.app"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
